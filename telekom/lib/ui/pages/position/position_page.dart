55e55854463589408e170b36b35f6c970f075b4d
p4lm4d3v <p4lm4d3v@gmail.com>
2024-08-01 23:29:12 +0200

    extracted the DataContainerRow widget to a separate file

--- telekom/lib/ui/pages/position/position_page.dart
+++ telekom/lib/ui/pages/position/position_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:numpad_layout/widgets/numpad.dart';
 import 'package:provider/provider.dart';
-import 'package:telekom/functions/width.dart';
 import 'package:telekom/logic/logic_provider.dart';
 import 'package:telekom/logic/colors/tia.dart';
 import 'package:telekom/static/std.dart';
+import 'package:telekom/ui/pages/position/data_container_row.dart';
 
 class PositionPage extends StatelessWidget {
   const PositionPage({super.key});
@@ -64,83 +64,6 @@ class PositionPage extends StatelessWidget {
               Std.space.H20,
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class DataContainerRow extends StatelessWidget {
-  const DataContainerRow({
-    super.key,
-    required this.dataName,
-    required this.data,
-    this.color,
-  });
-
-  final String dataName;
-  final String data;
-  final Color? color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: Std.padding.vertical5,
-      width: width(context, 1),
-      decoration: BoxDecoration(
-        color: Std.color.primary,
-        borderRadius: BorderRadius.circular(5),
-        boxShadow: [
-          BoxShadow(
-            color: Std.color.tertiary,
-            blurRadius: 2,
-            spreadRadius: 2.5,
-            offset: const Offset(0, 2.5),
-          )
-        ],
-      ),
-      child: Padding(
-        padding: Std.padding.all10,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              dataName,
-              textAlign: TextAlign.start,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  data,
-                  textAlign: TextAlign.start,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                Std.space.W10,
-                Visibility(
-                  visible: color != null && data != "_",
-                  child: Container(
-                    padding: Std.padding.all10,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: color ?? Std.color.white,
-                      border: Border.all(
-                        color: Std.color.black,
-                        width: 1.5,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
         ),
       ),
     );
